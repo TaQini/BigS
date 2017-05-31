e601c8851f9b92ca2e9a31da9e8c41735c11b0bb
TaQini <742954809@qq.com>
2017-05-31 11:23:43 +0800

    random food place. done. next:    Game control function.

--- mySnake.cpp
+++ mySnake.cpp
@@ -1,90 +1,13 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h> 
-#include <ctime>
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "conf.hpp"
+#include "node.hpp"
 using namespace std;
-
-// MAP SIZE
-#define MAP_WIDHT  16
-#define MAP_HEIGHT 16
-
-// DIRECTOR
-#define DIR_UP         0  
-#define DIR_DOWN       1  
-#define DIR_LEFT       2  
-#define DIR_RIGHT      3  
-
-// COLOR
-#define WHITE	1.0f,1.0f,1.0f
-#define YELLOW 	1.0f,1.0f,0.0f
-#define GREEN	0.0f,1.0f,0.0f
-
-// CLASS POS
-class POS{
-private:
-	int X,Y;
-public:
-	void SetPosition(int x, int y){	X = x; Y = y;}
-	int GetX() { return X;}
-	int GetY() { return Y;}
-};
-
-// CLASS RGB
-class RGB{
-private:
-	float R,G,B;
-public:
-	void SetColor(float r, float g, float b){ R = r; G = g; B = b;}
-};
-
-// CLASS NODE
-class Node : public RGB, public POS{
-private:
-	Node *fd;
-	Node *bk;
-public:
-	Node();
-	Node(int x, int y);
-	Node(float r, float g, float b);
-	Node(Node *fd, Node *bk);
-	void SetFD(Node *p);
-	void SetBK(Node *p);
-	Node *GetFD();
-	Node *GetBK();
-};
-Node::Node(){
-	SetFD(NULL);
-	SetBK(NULL);
-};
-Node::Node(int x, int y){
-	SetPosition(x,y);
-	SetFD(NULL);
-	SetBK(NULL);
-}
-Node::Node(float r, float g, float b){
-	SetColor(r,g,b);
-	SetFD(NULL);
-	SetBK(NULL);
-}
-Node::Node(Node *fd, Node *bk){
-	SetFD(fd);
-	SetBK(bk);
-}
-
-void Node::SetFD(Node *p){
-	fd = p;
-}
-Node *Node::GetFD(){
-	return fd;
-}
-Node *Node::GetBK(){
-	return bk;
-}
-
-void Node::SetBK(Node *p){
-	bk = p;
-}
 
 // CLASS SNAKE
 class Snake{
@@ -97,7 +20,9 @@ public:
 	void Insert(Node *head, Node *p);
 	// void Delete(int n); In ver.2: Eat special bean then cut down the length of snake	
 	void Move(/*DIR?*/);
-	
+	int CheckPos(int x, int y);
+	Node* GetHead(){ return head;}
+	Node* GetTail(){ return tail;}
 	void Show(){
 		Node *p = new(Node);
 		p = head;
@@ -107,6 +32,14 @@ public:
 		}
 	}
 };
+
+int Snake::CheckPos(int x, int y){
+	Node* p = new(Node);
+	for(p = head->GetFD(); p->GetX() != -1; p = p->GetFD()){
+		if( p->GetX() == x && p->GetY() == y ) return 0;
+	}
+	return 1;
+}
 
 Snake::Snake(int n){
 	length = n;
@@ -143,37 +76,70 @@ void Snake::Insert(Node *head, Node *p){
     p->GetFD()->SetBK(p);
 }
 
+
 class Food : public POS, public RGB{
-private:
+// private:
+	// int seed;
 public:
-	void RandomPlace(void);
-	void CheckPos(Snake &S);
+	void RandomPlace(Snake &S);
+	Food(){	}
+	int RandInt(int sz);
 };
-// void Food::CheckPos(Snake &S){
 
-// }
+int Food::RandInt(int sz){
+	char buf[4];
+	int fd = open("/dev/urandom",O_RDONLY,0); 
+	read(fd,buf,4);
+	close(fd);
+	return *(unsigned int*)buf % sz;
+}
 
-void Food::RandomPlace(void){
-    srand(time(NULL));
-    // do{
-	SetPosition(rand() % MAP_WIDHT, rand() % MAP_HEIGHT);
-    // }while (!CheckPos(x,y))
+void Food::RandomPlace(Snake &S){
+    do{
+		SetPosition( RandInt(MAP_WIDHT),RandInt(MAP_HEIGHT));
+    }while (!S.CheckPos(GetX(),GetY()));
     cout << GetX() << "," << GetY() << endl;
 }
 
 // CLASS DIR
-class Director{
+class Control{
+private:
+	int DIR;
+	int MOVE;
+	// int _dir_x[4] = {-1, 1, 0, 0};
+	// int _dir_y[4] = { 0, 0,-1, 1};  
+	int TIMER;
+	int OVER;  
+	int PAUSE;  
+
+public:
+	Control(int d){ 
+		ChangeDir(d); 
+		TIMER = TIME_BASE;
+		OVER = 0;
+		PAUSE = 0;
+	}
+	void ChangeDir(int d){ DIR = d;}
+	int GetDir(){ return DIR;}
+	void SpeedUp();
 
 };
-
-
+void Control::SpeedUp(){
+	if (TIMER >= TIME_MIN){
+		TIMER += TIME_UPUNIT;
+	}
+}
 
 int main(int argc, char **argv){
-	Snake BigS(8);
+	Snake BigS(INIT_LENGTH);
 	BigS.Show();
 	cout << "2333" << endl;
 	Food Beans;
-	Beans.RandomPlace();
+	Beans.RandomPlace(BigS);
+	Control Direction(DIR_RIGHT);
+
+	// CheckPos(BigS);
+	// Beans.RandomPlace();
 	// BigS.Initilize(5);
 	// Beans.RandomPlace();
 
